45b7149052185e9a94cd50b00ece088857c53097
latipjs <107148764+latipjs@users.noreply.github.com>
2022-06-16 16:41:02 +0300

    Add files via upload

---
+++ precache-manifest.c2f8efa5bc00fa7c388aa3b6aa3a147f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fbc2d1bef85ed6be17",
+    "url": "css/app.89a09e11.css"
+  },
+  {
+    "revision": "5d21081c795c99c337ad",
+    "url": "css/chunk-28ec3019.f92371e8.css"
+  },
+  {
+    "revision": "868eae1fc26987606263120f1c05e1b7",
+    "url": "index.html"
+  },
+  {
+    "revision": "f0fbc2d1bef85ed6be17",
+    "url": "js/app.a6461db7.js"
+  },
+  {
+    "revision": "5d21081c795c99c337ad",
+    "url": "js/chunk-28ec3019.ed32e93f.js"
+  },
+  {
+    "revision": "2ac02d09a8ca02f1866d",
+    "url": "js/chunk-vendors.accd8a99.js"
+  },
+  {
+    "revision": "4191172b0820dd10e328d3a0769dc3ac",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
